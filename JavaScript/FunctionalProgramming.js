a9cd864c3079345f6ccf94baa7147a881267957d
HarisFaris17 <harisfaris17@gmail.com>
2022-08-22 16:36:55 +0700

    Menambahkan immutability dalam FP

--- JavaScript/FunctionalProgramming.js
+++ JavaScript/FunctionalProgramming.js
@@ -1,4 +1,15 @@
-// pure function adalah fungsi yang tidak boleh ada interupsi dari maupun ke luar fungsi selain yang didefinisikan oleh argumen/parameter
+//konsep paradigma functional programming adalah konsep dimana membangun program secara deklaratif dibandingkan imperatif (dapat diartikan menggunakan fungsi yang sudah ada dibandingkan membangun sendiri program yang pada dasarnya mengambil line code yang lebih panjang seperti menggunakan operasi loop dan sebagainya)
+// pure function adalah fungsi yang tidak boleh bergantung terhadap objek di luar fungsi selain yang didefinisikan oleh argumen/parameter dan tidak boleh mengubah parameter fungsi
+// impure function
+var pi=3.14
+const luas_lingkaran=(jarijari)=>{
+    return pi*jarijari*jarijari;
+}
+console.log(luas_lingkaran(1));
+//tidak sengaja mengubah pi
+pi=5
+console.log(luas_lingkaran(1))
+
 var objectku="hehe"
 
 // impure function
@@ -25,3 +36,44 @@ function mengembalikan_tanpa_object(){
 }
 
 console.log(mengembalikan_tanpa_object());
+
+
+let person = {name:"Haris"};
+// impure function karena mengubah parameter person
+function createNewPerson(age,person){
+    //mengubah objek person di atas
+    person.age=age
+    return person;
+}
+
+let newPerson = createNewPerson(18,person);
+console.log(newPerson);
+console.log(person);
+
+//dalam functional programming lebih baik mengembalikan objek baru tanpa variabel untuk menghindari penggunaan objek di luar fungsi
+
+function createNewPersonWithAge(age,person){
+    return {...person,age};
+}
+let personku={name:"Nuke"}
+let personBaruTheReal=createNewPersonWithAge(18,personku);
+console.log(personBaruTheReal);
+
+//immutability adalah konsep dalam paradigma FP dimana fungsi tidak boleh mengubah property apapun dalam objek yang dijadikan argumen
+let kkkkkkkk={firstName: 'aha',lastName:"mmm"}
+// fungis yang tidak diizinkan dalam FP
+function changeLastName(lastName,person){
+    person.lastName=lastName;
+}
+
+changeLastName("Prasetyo",kkkkkkkk);
+console.log(kkkkkkkk);
+
+//lebih baik menggunakan fungsi yang mengembalikan objek baru namun dengan property baru dari argumen yang ingin diubah
+let personTest = {firstName: 'test',lastName:"mistake"}
+function createNewPersonWithLastNameChanged(lastName,person){
+    return {...person,lastName:lastName};
+}
+
+let newPersonLastNameChanged = createNewPersonWithLastNameChanged("true",personTest);
+console.log(newPersonLastNameChanged)
